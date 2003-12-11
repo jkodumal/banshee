0b1a6b34a9f99f8ba5ac26631d12cd020b2d22c8
jkodumal <jkodumal@c3c2f706-f20d-0410-965c-b05b340ef3e4>
2003-12-11 03:51:01 +0000

    Test for union find backtracking
    git-svn-id: https://banshee.svn.sourceforge.net/svnroot/banshee/trunk@33 c3c2f706-f20d-0410-965c-b05b340ef3e4

---
+++ banshee/tests/uf-bt-test.c
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2000-2004
+ *      The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+/* Test the union find backtracking functionality */
+#include <assert.h>
+#include "ufind.h"
+
+DECLARE_UFIND(elt,int);
+DEFINE_UFIND(elt,int);
+
+int main()
+{
+  elt e1,e2,e3,e4;
+  region scratchregion;
+  region_init();
+  uf_init();
+
+  scratchregion = newregion();
+
+  e1 = new_elt(scratchregion,1);
+  e2 = new_elt(scratchregion,2);
+  e3 = new_elt(scratchregion,3);
+  e4 = new_elt(scratchregion,4);
+
+  elt_union(e1,e2);
+  assert(elt_get_info(e2) == elt_get_info(e1));
+  uf_backtrack();
+  assert(elt_get_info(e1) == 1);
+  assert(elt_get_info(e2) == 2);
+  elt_union(e1,e2);
+  elt_union(e2,e3);
+  elt_union(e3,e4);
+
+  assert(elt_get_info(e1) == elt_get_info(e4));
+  
+  uf_backtrack();
+  assert(elt_get_info(e4) == 4);
+
+}
