fc6ed6ec188d5635b597ef6ddead8e566019c0bf
jkodumal <jkodumal@c3c2f706-f20d-0410-965c-b05b340ef3e4>
2004-09-29 19:52:10 +0000

    Script that mimics gcc, taking input files and appending to them a constant string that is the source file name
    git-svn-id: https://banshee.svn.sourceforge.net/svnroot/banshee/trunk@185 c3c2f706-f20d-0410-965c-b05b340ef3e4

---
+++ banshee/experiments/gcc_subst.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import sys
+import os
+import string
+
+def is_c_file(nextarg):
+    return (os.path.isfile(nextarg) and nextarg[-2:] == '.c' and not nextarg[0] == '-')
+
+def list_to_string_nolf(list):
+    if (len(list) == 0):
+	return ""
+    result = list[0]
+    for elt in list[1:]:
+	result = result + " " + elt
+    return result
+
+def modify_file(filename):
+#    os.system("echo \"#ifndef CANON_IDENT\" >> %s" %filename)
+#    os.system("echo \"#define CANON_IDENT\" >> %s" %filename)
+    os.system("echo \"const char *CANON_IDENT = \\\"%s\\\";\" >> %s" % (filename, filename))
+#    os.system("echo \"#endif\" >> %s" %filename)
+
+def main():
+    for arg in sys.argv[1:]:
+	if (is_c_file(arg)):
+	    print ("%s is probably an input file... modifying" % arg)
+	    modify_file(arg)
+    print ("Invoking gcc as: gcc %s" % list_to_string_nolf(sys.argv[1:]))
+    os.system("gcc %s" % list_to_string_nolf(sys.argv[1:]))
+
+if __name__ == "__main__":
+    main()
