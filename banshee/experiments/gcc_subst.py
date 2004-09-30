57cde0ea0baea7e8dab0141805167af408b76b43
jkodumal <jkodumal@c3c2f706-f20d-0410-965c-b05b340ef3e4>
2004-09-30 06:27:09 +0000

    This seems to work... it handles those cases where somehow GCC is invoked on a file twice (maybe because of the cheesy way arguments are processed) by preventing redefinitions of the same string
    git-svn-id: https://banshee.svn.sourceforge.net/svnroot/banshee/trunk@192 c3c2f706-f20d-0410-965c-b05b340ef3e4

--- banshee/experiments/gcc_subst.py
+++ banshee/experiments/gcc_subst.py
@@ -15,11 +15,26 @@ def list_to_string_nolf(list):
 	result = result + " " + elt
     return result
 
+def get_canon_int(filename):
+    output = os.popen("md5 -s \"%s\"" % filename)
+    return output.readlines()[0].split()[3]
+    
+def already_added(appendline, filename):
+    output = os.popen("tail -1 %s" % filename)
+    lastline = output.readlines()[0][:-1]
+    if (lastline == appendline):
+	return True
+    return False
+
 def modify_file(filename):
-#    os.system("echo \"#ifndef CANON_IDENT\" >> %s" %filename)
-#    os.system("echo \"#define CANON_IDENT\" >> %s" %filename)
-    os.system("echo \"const char *CANON_IDENT_%d = \\\"CANON_IDENT_%s\\\";\" >> %s" % (random.randint(0,200000), filename, filename))
+#    os.system("echo \"#ifndef CANON_IDENT_%s\" >> %s" %(get_canon_int(filename),filename))
+#    os.system("echo \"#define CANON_IDENT_%s\" >> %s" %(get_canon_int(filename),filename))
+    appendline = "const char *CANON_IDENT_%s = \"CANON_IDENT_%s\";" % (get_canon_int(filename), filename)
+    if (not already_added(appendline,filename)):
+	os.system("echo \"const char *CANON_IDENT_%s = \\\"CANON_IDENT_%s\\\";\" >> %s" % (get_canon_int(filename), filename, filename))
 #    os.system("echo \"#endif\" >> %s" %filename)
+#    os.system("echo \"const char *CANON_IDENT_%d = \\\"CANON_IDENT_%s\\\";\" >> %s" % (random.randint(0,200000), filename, filename))
+
 
 def main():
     for arg in sys.argv[1:]:
