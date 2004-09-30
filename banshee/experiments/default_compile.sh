076af0168e056418b036d71da6429df69c574b29
jkodumal <jkodumal@c3c2f706-f20d-0410-965c-b05b340ef3e4>
2004-09-30 19:37:54 +0000

    update default compile script to use gcc_subst.py.
    Update readme
    
    
    git-svn-id: https://banshee.svn.sourceforge.net/svnroot/banshee/trunk@194 c3c2f706-f20d-0410-965c-b05b340ef3e4

--- banshee/experiments/default_compile.sh
+++ banshee/experiments/default_compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd $1
-./setup 
+./setup && ./configure CC="/Users/jkodumal/work/oink_all/banshee/experiments/gcc_subst.py"
 rm -rf tests
 rm -rf examples
 rm -rf experiments
-make clean all CC="gcc -P -save-temps"
+make clean all CC="/Users/jkodumal/work/oink_all/banshee/experiments/gcc_subst.py -P -save-temps"
