5d14a15df8c6315e644d7133f76ea9af535b0558
jkodumal <jkodumal@c3c2f706-f20d-0410-965c-b05b340ef3e4>
2004-09-30 19:44:38 +0000

    Clean up the gcc_subst.py script
    git-svn-id: https://banshee.svn.sourceforge.net/svnroot/banshee/trunk@196 c3c2f706-f20d-0410-965c-b05b340ef3e4

--- banshee/experiments/gcc_subst.py
+++ banshee/experiments/gcc_subst.py
@@ -4,9 +4,12 @@ import os
 import string
 import random
 
+# A file is a C source file if it ends in '.c', doesn't start with
+# '-', and corresponds to a file on disk
 def is_c_file(nextarg):
     return (os.path.isfile(nextarg) and nextarg[-2:] == '.c' and not nextarg[0] == '-')
 
+# Convert a list to a space-separated string
 def list_to_string_nolf(list):
     if (len(list) == 0):
 	return ""
@@ -15,10 +18,13 @@ def list_to_string_nolf(list):
 	result = result + " " + elt
     return result
 
-def get_canon_int(filename):
+# Given a filename, return a canonical string suitable for use as a C
+# identifier
+def get_canon_ident(filename):
     output = os.popen("md5 -s \"%s\"" % filename)
     return output.readlines()[0].split()[3]
     
+# Return true if the line 'appendline' was already appended to the file
 def already_added(appendline, filename):
     output = os.popen("tail -1 %s" % filename)
     lastline = output.readlines()[0][:-1]
@@ -26,22 +32,17 @@ def already_added(appendline, filename):
 	return True
     return False
 
+# Add a string variable declaration to the end of a file. If the file
+# already ends with such a declaration, this won't add another
 def modify_file(filename):
-#    os.system("echo \"#ifndef CANON_IDENT_%s\" >> %s" %(get_canon_int(filename),filename))
-#    os.system("echo \"#define CANON_IDENT_%s\" >> %s" %(get_canon_int(filename),filename))
-    appendline = "const char *CANON_IDENT_%s = \"CANON_IDENT_%s\";" % (get_canon_int(filename), filename)
+    appendline = "const char *CANON_IDENT_%s = \"CANON_IDENT_%s\";" % (get_canon_ident(filename), filename)
     if (not already_added(appendline,filename)):
-	os.system("echo \"const char *CANON_IDENT_%s = \\\"CANON_IDENT_%s\\\";\" >> %s" % (get_canon_int(filename), filename, filename))
-#    os.system("echo \"#endif\" >> %s" %filename)
-#    os.system("echo \"const char *CANON_IDENT_%d = \\\"CANON_IDENT_%s\\\";\" >> %s" % (random.randint(0,200000), filename, filename))
-
+	os.system("echo \"const char *CANON_IDENT_%s = \\\"CANON_IDENT_%s\\\";\" >> %s" % (get_canon_ident(filename), filename, filename))
 
 def main():
     for arg in sys.argv[1:]:
 	if (is_c_file(arg)):
-	    #print ("%s is probably an input file... modifying" % arg)
 	    modify_file(arg)
-	    #print ("Invoking gcc as: gcc %s" % list_to_string_nolf(sys.argv[1:]))
     os.system("gcc %s" % list_to_string_nolf(sys.argv[1:]))
 
 if __name__ == "__main__":
