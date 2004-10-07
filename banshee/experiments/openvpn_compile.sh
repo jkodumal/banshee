898d1e2cbd293bd7aebd7558a0b7ec8f8bf8bd75
jkodumal <jkodumal@c3c2f706-f20d-0410-965c-b05b340ef3e4>
2004-10-07 16:59:26 +0000

    A A
    git-svn-id: https://banshee.svn.sourceforge.net/svnroot/banshee/trunk@208 c3c2f706-f20d-0410-965c-b05b340ef3e4

--- banshee/experiments/openvpn_compile.sh
+++ banshee/experiments/openvpn_compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd $1
-autoreconf -i -v && ./configure --disable-ssl --disable-lzo CC="/moa/sc1/jkodumal/work/banshee/experiments/gcc_subst.py" &> ../buildlogs/openvpn_$2.confout
+autoreconf -i -v &> ../buildlogs/openvpn_$2.aconfout && ./configure --disable-ssl --disable-lzo CC="/moa/sc1/jkodumal/work/banshee/experiments/gcc_subst.py" &> ../buildlogs/openvpn_$2.confout
 make CC="/moa/sc1/jkodumal/work/banshee/experiments/gcc_subst.py -P -save-temps -D__extension__= " &> ../buildlogs/openvpn_$2.makeout
