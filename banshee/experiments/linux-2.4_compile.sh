7878971b5d8d2697235994c0e5a70715eb66a999
jkodumal <jkodumal@c3c2f706-f20d-0410-965c-b05b340ef3e4>
2004-10-15 21:46:39 +0000

    update-- use sh instead of bash
    git-svn-id: https://banshee.svn.sourceforge.net/svnroot/banshee/trunk@220 c3c2f706-f20d-0410-965c-b05b340ef3e4

--- banshee/experiments/linux-2.4_compile.sh
+++ banshee/experiments/linux-2.4_compile.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
-export GCC_SUBST=gcc296
-export CC="/moa/sc1/jkodumal/work/banshee/experiments/gcc_subst.py"
+#!/bin/sh
+setenv GCC_SUBST gcc296
 cd $1
-yes '' | make config &> ../buildlogs/linux-2.4_$2.confout 
-make dep &> ../buildlogs/linux-2.4_$2.depout 
-make &> ../buildlogs/linux-2.4_$2.buildout
-moke modules &> ../buildlogs/linux-2.4_$2.modout
-make modules_install &> ../buildlogs/linux-2.4_$2.modinstout
+chmod -R +w *
+yes '' | make config CC="/moa/sc1/jkodumal/work/banshee/experiments/gcc_subst.py -P -save-temps" &> ../buildlogs/linux-2.4_$2.confout  
+make dep CC="/moa/sc1/jkodumal/work/banshee/experiments/gcc_subst.py -P -save-temps"
+ &> ../buildlogs/linux-2.4_$2.depout 
+make CC="/moa/sc1/jkodumal/work/banshee/experiments/gcc_subst.py -P -save-temps"
+ &> ../buildlogs/linux-2.4_$2.buildout
+moke modules CC="/moa/sc1/jkodumal/work/banshee/experiments/gcc_subst.py -P -save-temps"
+ &> ../buildlogs/linux-2.4_$2.modout
+make modules_install CC="/moa/sc1/jkodumal/work/banshee/experiments/gcc_subst.py -P -save-temps"
+  &> ../buildlogs/linux-2.4_$2.modinstout
