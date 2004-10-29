0f2a48d653d3c38ad795e4d622f0532d6924a599
jkodumal <jkodumal@c3c2f706-f20d-0410-965c-b05b340ef3e4>
2004-10-29 22:52:59 +0000

    update
    git-svn-id: https://banshee.svn.sourceforge.net/svnroot/banshee/trunk@255 c3c2f706-f20d-0410-965c-b05b340ef3e4

--- banshee/experiments/default_compile.sh
+++ banshee/experiments/default_compile.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd $1
-rm -rf experiments/
+rm -rf experiments
+rm -rf config
+rm -rf examples
+rm -rf tests
 aclocal
 autoconf
 automake
 ./configure CC="/home/eecs/jkodumal/work/pldi05_experiments/banshee/experiments/gcc_subst.py"
-make clean all CC="/home/eecs/jkodumal/work/pldi05_experiments/banshee/experiments/gcc_subst.py"
+make clean all CC="/home/eecs/jkodumal/work/pldi05_experiments/banshee/experiments/gcc_subst.py -P -save-temps"
