c4367cc24922bb4264c3b158d15c2892e2798e35
jkodumal <jkodumal@c3c2f706-f20d-0410-965c-b05b340ef3e4>
2004-10-29 19:21:15 +0000

    Update default build process for beryl
    git-svn-id: https://banshee.svn.sourceforge.net/svnroot/banshee/trunk@249 c3c2f706-f20d-0410-965c-b05b340ef3e4

--- banshee/experiments/default_compile.sh
+++ banshee/experiments/default_compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd $1
-./setup && ./configure CC="/moa/sc1/jkodumal/work/banshee/experiments/gcc_subst.py"
-rm -rf tests
-rm -rf examples
-rm -rf experiments
-make clean all CC="/moa/sc1/jkodumal/work/banshee/experiments/gcc_subst.py -P -save-temps"
+aclocal
+autoconf
+automake
+./configure CC="/home/eecs/jkodumal/work/pldi05_experiments/banshee/experiments/gcc_subst.py"
+make clean all CC=CC="/home/eecs/jkodumal/work/pldi05_experiments/banshee/experiments/gcc_subst.py"
